02e90c5c5937e4b3ddaf1e0db4887aae9a4a1a96
S510U <fx0605@naver.com>
2024-05-21 23:01:43 +0900

    //0521

--- Codeup_C/1st_week.c
+++ Codeup_C/1st_week.c
@@ -1,15 +1,30 @@
 #include<stdio.h>
 
+// 기초1. 출력문
+// 
+//int main(void)
+//{
+//	printf("codeup c\n"); // start
+//
+//	printf("hello, world!\n"); // 1101
+//
+//	printf("hello,\nworld!\n"); // 1102
+//
+//	printf("\"c:\\test\"\n"); // 1103
+//
+//	printf("special characters\n");
+//	printf("[\\n,\\\",\\\\] is very important."); // 1731
+//
+//	return 0;
+//}
+
+// 기초2. 입출력문 및 연산자
+//
 int main(void)
 {
-	printf("Codeup C\n"); // start
-
-	printf("Hello, World!\n"); // 1101
-
-	printf("Hello,\nWorld!\n"); // 1102
-
-	printf("\"c:\\test\""); // 1103
-
+	int a;
+	scanf_s("%d", &a);
+	printf("%d", a);  // 1110
 
 	return 0;
 }
