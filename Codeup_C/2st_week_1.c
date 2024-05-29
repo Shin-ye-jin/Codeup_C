6fdb9f7bea2e894d52bf9775b9bdf620e5744966
S510U <fx0605@naver.com>
2024-05-29 12:01:47 +0900

    // 0529

---
+++ Codeup_C/2st_week_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d", a & b); // 1143
+//	return 0;
+//}
+
+int main(void)
+{
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	printf("%d", a | b); //1144
+	return 0;
+}
