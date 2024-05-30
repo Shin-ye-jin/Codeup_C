fb289cbc2e52a273087c45eef8dadcf1ad3fe60e
S510U <fx0605@naver.com>
2024-05-30 23:03:27 +0900

    // 0530

--- Codeup_C/2st_week_1.c
+++ Codeup_C/2st_week_1.c
@@ -4,7 +4,7 @@
 //{
 //	int a, b;
 //	scanf_s("%d %d", &a, &b);
-//	printf("%d", a & b); // 1143
+//	printf("%d", a & b); // 1143 비트 연산자(AND)
 //	return 0;
 //}
 
@@ -12,7 +12,7 @@
 //{
 //	int a, b;
 //	scanf_s("%d %d", &a, &b);
-//	printf("%d", a | b); //1144
+//	printf("%d", a | b); //1144 비트 연산자(OR)
 //	return 0;
 //}
 
@@ -20,7 +20,7 @@
 //{
 //	int a, b;
 //	scanf_s("%d %d", &a, &b);
-//	printf("%d", a << b); // 1147
+//	printf("%d", a << b); // 1147 비트 연산자(<<)
 //	return 0;
 //}
 
@@ -28,7 +28,7 @@
 //{
 //	int a, b;
 //	scanf_s("%d %d", &a, &b);
-//	printf("%d", a >> b); // 1148
+//	printf("%d", a >> b); // 1148 비트 연산자(>>)
 //	return 0;
 //}
 
@@ -39,28 +39,28 @@
 //	if (a > b)
 //		printf("%d", a);
 //	else
-//		printf("%d", b); // 1149
+//		printf("%d", b); // 1149 두 수 중 큰 수
 //	return 0;
 //}
 
-int main(void)
-{
-	int a, b, c;
-	scanf_s("%d %d %d", &a, &b, &c);
-	if (a > b)
-		if (b > c)
-			printf("%d", c);
-		else
-			printf("%d", b);
-	else if (a > c)
-		if (c > b)
-			printf("%d", b);
-		else
-			printf("%d", c);
-	else
-		if (c > a)
-			printf("%d", a);
-		else
-			printf("%d", c); // 1150
-	return 0;
-}
+//int main(void)
+//{
+//	int a, b, c;
+//	scanf_s("%d %d %d", &a, &b, &c);
+//	if (a > b)
+//		if (b > c)
+//			printf("%d", c);
+//		else
+//			printf("%d", b);
+//	else if (a > c)
+//		if (c > b)
+//			printf("%d", b);
+//		else
+//			printf("%d", c);
+//	else
+//		if (c > a)
+//			printf("%d", a);
+//		else
+//			printf("%d", c); // 1150 세 수 중 가장 작은 수
+//	return 0;
+//}
