ff58f23250df8f0fdb302ce2510cc179e03a36e5
S510U <fx0605@naver.com>
2024-05-30 22:31:22 +0900

    // 0530

--- Codeup_C/2st_week_1.c
+++ Codeup_C/2st_week_1.c
@@ -8,10 +8,59 @@
 //	return 0;
 //}
 
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d", a | b); //1144
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d", a << b); // 1147
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d", a >> b); // 1148
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	if (a > b)
+//		printf("%d", a);
+//	else
+//		printf("%d", b); // 1149
+//	return 0;
+//}
+
 int main(void)
 {
-	int a, b;
-	scanf_s("%d %d", &a, &b);
-	printf("%d", a | b); //1144
+	int a, b, c;
+	scanf_s("%d %d %d", &a, &b, &c);
+	if (a > b)
+		if (b > c)
+			printf("%d", c);
+		else
+			printf("%d", b);
+	else if (a > c)
+		if (c > b)
+			printf("%d", b);
+		else
+			printf("%d", c);
+	else
+		if (c > a)
+			printf("%d", a);
+		else
+			printf("%d", c); // 1150
 	return 0;
 }
