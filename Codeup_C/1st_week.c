f93e2fbe830050e1902cee36c55f53ece5b405b0
S510U <fx0605@naver.com>
2024-05-30 22:58:14 +0900

    // 0530 주석

--- Codeup_C/1st_week.c
+++ Codeup_C/1st_week.c
@@ -6,14 +6,14 @@
 //{
 //	printf("codeup c\n"); // start
 //
-//	printf("hello, world!\n"); // 1101
+//	printf("hello, world!\n"); // 1101 Hello,World!
 //
-//	printf("hello,\nworld!\n"); // 1102
+//	printf("hello,\nworld!\n"); // 1102 Hello,World!(줄바꿈)
 //
-//	printf("\"c:\\test\"\n"); // 1103
+//	printf("\"c:\\test\"\n"); // 1103 폴더명 출력
 //
 //	printf("special characters\n");
-//	printf("[\\n,\\\",\\\\] is very important."); // 1731
+//	printf("[\\n,\\\",\\\\] is very important."); // 1731 특수 서식 문자 출력하기
 //
 //	return 0;
 //}
@@ -24,18 +24,9 @@
 //{
 //	int a;
 //	scanf_s("%d", &a);
-//	// printf("%d", a);  // 1110
+//	// printf("%d", a);  // 1110 정수 그래도 출력하기
 //
-//	// printf("%d%%", a); // 1111
-//
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	int a, b;
-//	scanf_s("%d %d", &a, &b);
-//	printf("%d %d", a, b); // 1112
+//	// printf("%d%%", a); // 1111 % 출력
 //
 //	return 0;
 //}
@@ -44,7 +35,16 @@
 //{
 //	int a, b;
 //	scanf_s("%d %d", &a, &b);
-//	printf("%d %d", b, a); // 1113
+//	printf("%d %d", a, b); // 1112 두 정수 출력
+//
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d %d", b, a); // 1113 바꿔서 출력하기
 //	return 0;
 //}
 
@@ -53,14 +53,14 @@
 //	int a, b;
 //	scanf_s("%d %d", &a, &b);
 //	printf("%d", a + b);
-//	return 0; // 1114
+//	return 0; // 1114 두 정수의 덧셈
 //}
 
 //int main(void)
 //{
 //	long long int a, b;
 //	scanf_s("%lld %lld", &a, &b);
-//	printf("%lld", a + b); // 1115
+//	printf("%lld", a + b); // 1115 두 정수의 덧셈(64비트)
 //	return 0;
 //}
 
@@ -71,7 +71,7 @@
 //	printf("%d+%d=%d\n", a, b, a + b);
 //	printf("%d-%d=%d\n", a, b, a - b);
 //	printf("%d*%d=%d\n", a, b, a * b);
-//	printf("%d/%d=%d\n", a, b, a / b);  // 1116
+//	printf("%d/%d=%d\n", a, b, a / b);  // 1116 사칙연산 계산기
 //	return 0;
 //}
 
@@ -79,7 +79,7 @@
 //{
 //	float a, b;
 //	scanf_s("%f %f", &a, &b);
-//	printf("%.2f", a * b); // 1117
+//	printf("%.2f", a * b); // 1117 두 실수의 곱
 //	return 0;
 //}
 
@@ -88,7 +88,7 @@
 //	float a, b, w;
 //	scanf_s("%f %f", &a, &b);
 //	w = a * b / 2;
-//	printf("%.1f", w); // 1118
+//	printf("%.1f", w); // 1118 삼각형의 넓이 구하기
 //	return 0;
 //}
 
@@ -96,7 +96,7 @@
 //{
 //	int a;
 //	scanf_s("%d", &a);
-//	printf("%d", a * 24); // 1119
+//	printf("%d", a * 24); // 1119 일을 시간으로 변환
 //	return 0;
 //}
 
@@ -106,7 +106,7 @@
 //	float avg = 0;
 //	scanf_s("%d %d %d", &a, &b, &c);
 //	avg = (float)(a + b + c) / 3.0;
-//	printf("%.2f", avg); // 1120
+//	printf("%.2f", avg); // 1120 세수의 평균
 //	return 0;
 //}
 
@@ -114,7 +114,7 @@
 //{
 //	int a, b;
 //	scanf_s("%d %d", &a, &b);
-//	printf("%d", a % b); // 1121
+//	printf("%d", a % b); // 1121 나머지 구하기
 //	return 0;
 //}
 
@@ -122,7 +122,7 @@
 //{
 //	int a;
 //	scanf_s("%d", &a);
-//	printf("%d %d", a / 60, a % 60); // 1122
+//	printf("%d %d", a / 60, a % 60); // 1122 초를 분/초로 변환
 //	return 0;
 //}
 
@@ -132,7 +132,7 @@
 //	float f;
 //	scanf_s("%d", &a);
 //	f = (float) 9 / 5 * a + 32;
-//	printf("%.3f", f); // 1123
+//	printf("%.3f", f); // 1123 섭씨 온도를 화씨 온도로 변환
 //	return 0;
 //}
 
@@ -140,6 +140,6 @@
 //{
 //	int a;
 //	scanf_s("%d", &a);
-//	printf("%o %X", a, a); // 1125
+//	printf("%o %X", a, a); // 1125 8진수 16진수 변환
 //	return 0;
 //}
