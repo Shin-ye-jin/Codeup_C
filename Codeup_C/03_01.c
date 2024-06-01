dc30dbae0a3352dbe596393f85fe5685ff712397
S510U <fx0605@naver.com>
2024-06-01 11:15:01 +0900

    // 0601

--- Codeup_C/03_01.c
+++ Codeup_C/03_01.c
@@ -23,15 +23,48 @@
 //	return 0; // 1151 10보다 작은 수 (else 버전)
 //}
 
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	if (a > b)
+//		printf(">");
+//	else if (b > a)
+//		printf("<");
+//	else if (a == b)
+//		printf("=");
+//	return 0; // 1152 두 수의 대소 비교
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	if (a > b)
+//		printf("%d", a - b);
+//	else
+//		printf("%d", b - a);
+//	return 0; // 1154 큰수 - 작은수
+//}
+
+//int main(void)
+//{
+//	int a;
+//	scanf_s("%d", &a);
+//	if (a % 7 == 0)
+//		printf("multiple");
+//	else
+//		printf("not multiple");
+//	return 0; // 1155 7의 배수
+//}
+
 int main(void)
 {
-	int a, b;
-	scanf_s("%d %d", &a, &b);
-	if (a > b)
-		printf(">");
-	else if (b > a)
-		printf("<");
-	else if (a == b)
-		printf("=");
-	return 0; // 1152 두 수의 대소 비교
+	int a;
+	scanf_s("%d", &a);
+	if (a % 2 == 0)
+		printf("even");
+	else
+		printf("odd");
+	return 0; // 1156 홀수 짝수 구별
 }
