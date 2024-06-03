716eb27f82042f588910345de3ecbf782abd03a8
S510U <fx0605@naver.com>
2024-06-03 17:38:59 +0900

    // 0603

---
+++ Codeup_C/03_03.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int a;
+//	scanf_s("%d", &a);
+//
+//	if ((a == 1) || (a == 3) || (a == 5) || (a == 7))
+//		printf("oh my god");
+//	else
+//		printf("enjoy");
+//
+//	return 0; // 1160 ¾Æ¸£¹ÙÀÌÆ® °¡´Â ³¯
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//
+//	if (a % 2 == 0)
+//		if (b % 2 == 0)
+//			printf("Â¦¼ö+Â¦¼ö=Â¦¼ö");
+//		else
+//			printf("Â¦¼ö+È¦¼ö=È¦¼ö");
+//	else
+//		if (b % 2 == 0)
+//			printf("È¦¼ö+Â¦¼ö=È¦¼ö");
+//		else
+//			printf("È¦¼ö+È¦¼ö=Â¦¼ö");
+//
+//	return 0; // 1161 È¦¼ö¿Í Â¦¼ö ±×¸®°í ´õÇÏ±â
+//}
+
+//int main(void)
+//{
+//	int y, m, d;
+//	scanf_s("%d %d %d", &y, &m, &d);
+//
+//	if ((y - m + d)%10 ==0)
+//		printf("´ë¹Ú");
+//	else
+//		printf("±×·°Àú·°");
+//
+//	return 0; // 1162 ´ç½ÅÀÇ »çÁÖ¸¦ ºÁ µå¸³´Ï´Ù 1
+//}
+
