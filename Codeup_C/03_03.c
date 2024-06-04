7ecbdb95abe08fc9a88ad5b827259b9e4fe75193
S510U <fx0605@naver.com>
2024-06-05 00:19:55 +0900

    // 0604

--- Codeup_C/03_03.c
+++ Codeup_C/03_03.c
@@ -45,3 +45,37 @@
 //	return 0; // 1162 당신의 사주를 봐 드립니다 1
 //}
 
+//int main(void)
+//{
+//	int y, m, d;
+//	scanf_s("%d %d %d", &y, &m, &d);
+//	
+//	if ((y + m + d) / 100 % 2 == 0)
+//		printf("대박");
+//	else
+//		printf("그럭저럭");
+//
+//	return 0; // 당신의 사주를 봐 드립니다 2
+//}
+
+//int main(void)
+//{
+//	int a, b, c;
+//	scanf_s("%d %d %d", &a, &b, &c);
+//
+//	if (a > 170 && b > 170 && c > 170)
+//		printf("PASS");
+//	else
+//		printf("CRASH");
+//
+//	return 0; // 터널 통과하기 1
+//}
+
+int main(void)
+{
+	int time, count;
+	scanf_s("%d %d", &time, &count);
+
+	printf("%d", (89 - time) / 5 + 1 + count);
+	return 0; // 1165 축구의 신 1
+}
