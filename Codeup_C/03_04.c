e6e1f49b665a4ab1eb335c7237c556cbec0f85c4
S510U <fx0605@naver.com>
2024-06-10 00:06:29 +0900

    // 0606

--- Codeup_C/03_04.c
+++ Codeup_C/03_04.c
@@ -53,3 +53,59 @@
 //
 //    return 0; // 1168 나이 계산 1
 //}
+
+//int main(void)
+//{
+//    int a, num = 0;
+//    scanf("%d", &a);
+//    num = a - 13;
+//
+//    if (num > 0) {
+//        printf("%d %d", 100 - num, 1);
+//    }
+//    else {
+//        printf("%d %d", 0 - num, 3);
+//    }
+//
+//    return 0; //1169 나이 계산 2
+//}
+
+//int main(void)
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//
+//    if (c < 10) {
+//        printf("%d%d0%d", a, b, c);
+//    }
+//    else {
+//        printf("%d%d%d", a, b, c);
+//    }
+//
+//    return 0; // 1170 당신의 학번은? 1
+//}
+
+//int main(void)
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//
+//    if (b < 10) {
+//        if (c < 10)
+//            printf("%d0%d00%d", a, b, c);
+//        else if (c < 100)
+//            printf("%d0%d0%d", a, b, c);
+//        else
+//            printf("%d0%d%d", a, b, c);
+//    }
+//    else {
+//        if (c < 10)
+//            printf("%d%d00%d", a, b, c);
+//        else if (c < 100)
+//            printf("%d%d0%d", a, b, c);
+//        else
+//            printf("%d%d%d", a, b, c);
+//    }
+//
+//    return 0; // 1171 당신의 학번은? 2
+//}
