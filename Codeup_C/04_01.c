d123be8ce8bda681250f35149251206a2b4a85fa
S510U <fx0605@naver.com>
2024-06-11 00:10:39 +0900

    // 0610

---
+++ Codeup_C/04_01.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int a, b, c, d, sum = 0;
+//	scanf_s("%d %d %d %d", &a, &b, &c, &d);
+//	sum = a + b + c + d;
+//
+//	if (sum == 0)
+//		printf("¸ð");
+//	else if (sum == 1)
+//		printf("µµ");
+//	else if (sum == 2)
+//		printf("°³");
+//	else if (sum == 3)
+//		printf("°É");
+//	else if (sum == 4)
+//		printf("À·");
+//	return 0; // À·³îÀÌ
+//}
+
+//int main(void)
+//{
+//	int a, b,sum=0;
+//	scanf_s("%d %d", &a, &b);
+//
+//	if (a == 1) sum += 400;
+//	if (a == 2) sum += 340;
+//	if (a == 3) sum += 170;
+//	if (a == 4) sum += 100;
+//	if (a == 5) sum += 70;
+//
+//	if (b == 1) sum += 400;
+//	if (b == 2) sum += 340;
+//	if (b == 3) sum += 170;
+//	if (b == 4) sum += 100;
+//	if (b == 5) sum += 70;
+//
+//	if (sum > 500)
+//		printf("angry");
+//	else
+//		printf("no angry");
+//
+//	return 0; // 1210 Ä®·Î¸® °è»êÇÏ±â
+//}
+
+int main(void)
+{
+	int a, b, c;
+	scanf_s("%d %d %d", &a, &b, &c);
+
+	if (a >= b && a >= c)
+		if (a < b + c)
+			printf("yes");
+		else
+			printf("no");
+	else if (b >= a && b >= c)
+		if (b < a + c)
+			printf("yes");
+		else
+			printf("no");
+	else if (c >= a && c >= b)
+		if (c < a + b)
+			printf("yes");
+		else
+			printf("no");
+
+	return 0; // 1212 »ï°¢ÇüÀÇ ¼º¸³ Á¶°Ç
+}
