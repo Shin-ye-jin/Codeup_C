d92da6a465a3262a12518e039972acc163d34d51
S510U <fx0605@naver.com>
2024-06-12 17:48:06 +0900

    // 0612

--- Codeup_C/04_02.c
+++ Codeup_C/04_02.c
@@ -50,3 +50,73 @@
 //
 //	return 0; // 1218 삼각형 판단하기
 //}
+
+//int main(void)
+//{
+//	int a, b, c, count = 0;
+//	scanf_s("%d %d %d", &a, &b, &c);
+//
+//	count = (89 - a) / 5 + 1;
+//	if (count + b > c)
+//		printf("win");
+//	else if (count + b == c)
+//		printf("same");
+//	else
+//		printf("lose");
+//
+//	return 0; // 1222 축구의 신 2
+//}
+
+//int main(void)
+//{
+//	float a, b, c, d;
+//	scanf_s("%f %f %f %f", &a, &b, &c, &d);
+//
+//	if (a / b > c / d)
+//		printf(">");
+//	else if (a / b == c / d)
+//		printf("=");
+//	else
+//		printf("<");
+//
+//	return 0; // 분수 크기 비교
+//}
+
+//int main(void)
+//{
+//	int a, b, c, d, e, f, g;
+//	int num1, num2, num3, num4, num5, num6;
+//	int count = 0, bonus = 0;
+//	scanf_s("%d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g);
+//	scanf_s("%d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6);
+//
+//	if (num1 == a || num1 == b || num1 == c || num1 == d || num1 == e || num1 == f)
+//		count += 1;
+//	if (num2 == a || num2 == b || num2 == c || num2 == d || num2 == e || num2 == f)
+//		count += 1;
+//	if (num3 == a || num3 == b || num3 == c || num3 == d || num3 == e || num3 == f)
+//		count += 1;
+//	if (num4 == a || num4 == b || num4 == c || num4 == d || num4 == e || num4 == f)
+//		count += 1;
+//	if (num5 == a || num5 == b || num5 == c || num5 == d || num5 == e || num5 == f)
+//		count += 1;
+//	if (num6 == a || num6 == b || num6 == c || num6 == d || num6 == e || num6 == f)
+//		count += 1;
+//	if (num1 == g || num2 == g || num3 == g || num4 == g || num5 == g || num6 == g)
+//		bonus += 1;
+//
+//	if (count == 6)
+//		printf("1");
+//	else if (count == 5 && bonus == 1)
+//		printf("2");
+//	else if (count == 5)
+//		printf("3");
+//	else if (count == 4)
+//		printf("4");
+//	else if (count == 3)
+//		printf("5");
+//	else
+//		printf("0");
+//	
+//	return 0;// 1226 이번 주 로또
+//}
