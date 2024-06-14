05f71b04f9e73d7af832a7b5669dd8652b29c90a
S510U <fx0605@naver.com>
2024-06-14 16:43:28 +0900

    // 0614

---
+++ Codeup_C/04_04.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int i;
+//	for (i = 1; i <= 100; i++)
+//		printf("%d ", i);
+//	return 0; //1251 1부터 100까지 출력하기
+//
+//}
+
+//int main(void)
+//{
+//	int i, n;
+//	scanf("%d", &n);
+//
+//	for (i = 1; i < n + 1; i++)
+//		printf("%d ", i);
+//
+//	return 0; // 1252 1부터 n까지 출력하기
+//}
+
+//int main(void)
+//{
+//	int a, b, i;
+//	scanf("%d %d", &a, &b);
+//
+//	if (a > b) {
+//		for (i = b; i < a + 1; i++)
+//			printf("%d ", i);
+//	}
+//	else if (b > a) {
+//		for (i = a; i < b + 1; i++)
+//			printf("%d ", i);
+//	}
+//	else {
+//		for (i = a; i < b + 1; i++)
+//			printf("%d ", i);
+//	}
+//
+//	return 0; // 1253 a부터 b까지 출력하기
+//}
