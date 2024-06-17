366a4bec055987302d67b279c3176e675572dcc0
S510U <fx0605@naver.com>
2024-06-17 19:49:25 +0900

    // 0617

---
+++ Codeup_C/05_01.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+//int main(void)
+//{
+//	int a, b, sum = 0, i;
+//	scanf("%d %d", &a, &b);
+//
+//	for (i = a; i <= b; i++) {
+//		if (i % 3 == 0)
+//			sum += i;
+//	}
+//	printf("%d", sum);
+//	return 0; // 1260 3의 배수의 합
+//}
+
+//int main(void)
+//{
+//	int num, i,count=0;
+//	for (i = 0; i < 10; i++) {
+//		scanf("%d", &num);
+//		if (num % 5 == 0) {
+//			printf("%d", num);
+//			count++;
+//			break;
+//		}
+//	}
+//
+//	if (count == 0)
+//		printf("0");
+//	return 0; // 1261 First Speical Judge(Test)
+//}
+
+//int main(void)
+//{
+//	int num, i;
+//	scanf("%d", &num);
+//	
+//	for (i = 1; i <= 9; i++) {
+//		printf("%d*%d=%d\n", num, i, num * i);
+//	}
+//	return 0; // 1265 구구단 출력하기 1
+//}
+
