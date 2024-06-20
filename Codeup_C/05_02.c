14a5fef6b6db2bd7d15ff9124af13d85aa96e9e9
S510U <fx0605@naver.com>
2024-06-20 22:38:03 +0900

    // 0619

---
+++ Codeup_C/05_02.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main(void)
+//{
+//    int a, b, c, n, i;
+//    scanf("%d %d %d %d", &a, &b, &c, &n);
+//
+//    for (i = 2; i <= n; i++) {
+//        a = a * b + c;
+//    }
+//    printf("%d", a);
+//    return 0; // 1269 수열의 값 구하기
+//}
+
+//int main(void) {
+//    int num, i, count = 0;
+//    scanf("%d", &num);
+//
+//    for (i = 1; i <= num; i++) {
+//        if (i % 10 == 1) {
+//            count++;
+//        }
+//    }
+//    printf("%d", count);
+//    return 0; // 1270 1의 개수는? 1
+//}
+
+//int main(void)
+//{
+//    int n, num, max = 0, i;
+//    scanf("%d", &n);
+//
+//    for (i = 0; i < n; i++) {
+//        scanf("%d", &num);
+//        if (num > max) {
+//            max = num;
+//        }
+//    }
+//    printf("%d", max);
+//    return 0; // 1271 최댓값 구하기
+//}
