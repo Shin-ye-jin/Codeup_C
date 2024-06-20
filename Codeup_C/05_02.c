66fd59e69f870d2a1a93b71e16f26096122c3ce5
S510U <fx0605@naver.com>
2024-06-20 22:40:21 +0900

    // 0620

--- Codeup_C/05_02.c
+++ Codeup_C/05_02.c
@@ -40,3 +40,56 @@
 //    printf("%d", max);
 //    return 0; // 1271 최댓값 구하기
 //}
+
+//int main(void)
+//{
+//    int i, n1, n2, sum = 0;
+//    scanf("%d %d", &n1, &n2);
+//
+//    if (n1 % 2 == 1) {
+//        sum += n1 / 2 + 1;
+//    }
+//    else {
+//        sum += n1 * 5;
+//    }
+//    if (n2 % 2 == 1) {
+//        sum += n2 / 2 + 1;
+//    }
+//    else {
+//        sum += n2 * 5;
+//    }
+//    printf("%d", sum);
+//    return 0; // 1272 기부
+//}
+
+//int main(void)
+//{
+//    int num, i;
+//    scanf("%d", &num);
+//
+//    for (i = 1; i <= num; i++) {
+//        if (num % i == 0) {
+//            printf("%d ", i);
+//        }
+//    }
+//    return 0; // 1273 약수 구하기
+//}
+
+//int main(void)
+//{
+//    int num, i, count = 0;
+//    scanf("%d", &num);
+//
+//    for (i = 2; i < num; i++) {
+//        if (num % i == 0) {
+//            count++;
+//        }
+//    }
+//
+//    if (count == 0)
+//        printf("prime");
+//    else
+//        printf("not prime");
+//
+//    return 0; // 1274 소수 판별
+//}
