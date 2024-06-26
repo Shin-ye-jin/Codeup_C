2d0c2bfa018bb611c4fb2a83a52d0e83593a4c43
S510U <fx0605@naver.com>
2024-06-27 00:46:28 +0900

    // 0628

---
+++ Codeup_C/06_03.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int n, i, j;
+//	scanf("%d", &n);
+//
+//	for (i = 1; i <= n; i++) {
+//		for (j = 0; j < i; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = n; i > 0; i--) {
+//		for (j = 1; j < i; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0; // 1357 삼각형 출력하기 4
+//}
