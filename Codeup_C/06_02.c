be2a8f3410053a4db3e1c45f6321b650dfd53728
S510U <fx0605@naver.com>
2024-06-27 00:16:57 +0900

    // 0626

---
+++ Codeup_C/06_02.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int a, b, i, j;
+//	scanf("%d %d", &a, &b);
+//
+//	for (i = a; i <= b; i++) {
+//		for (j = 1; j <= 9; j++) {
+//			printf("%d*%d=%d\n", i, j, i * j);
+//		}
+//	}
+//	return 0; //1351 구구단 출력하기 2
+//}
+
+//int main(void)
+//{
+//	int n, i, j;
+//	scanf("%d", &n);
+//
+//	for (i = 0; i < n; i++) {
+//		for (j = 0; j < n; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	
+//	return 0;// 1352 사각형 출력하기 1
+//}
+
+//int main(void)
+//{
+//	int n, i, j;
+//	scanf("%d", &n);
+//
+//	for (i = 1; i <= n; i++) {
+//		for (j = 0; j < i; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0; // 1353 삼각형 출력하기 1
+//}
