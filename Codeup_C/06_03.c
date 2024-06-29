4deab699aa333899e1480f0898bc241751e69e58
S510U <fx0605@naver.com>
2024-06-29 12:28:19 +0900

    // 0628

--- Codeup_C/06_03.c
+++ Codeup_C/06_03.c
@@ -20,3 +20,36 @@
 //	}
 //	return 0; // 1357 삼각형 출력하기 4
 //}
+
+//int main(void)
+//{
+//	int n, i, j;
+//	scanf("%d", &n);
+//
+//	for (i = 0; i < n / 2 + 1; i++) {
+//		for (j = 0; j < n / 2-i; j++) {
+//			printf(" ");
+//		}
+//		for (j = 0; j < i*2+1; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0; // 1358 삼각형 출력하기 5
+//}
+
+//int main(void) {
+//	int n, i, j;
+//	scanf("%d", &n);
+//
+//	for (i = 0; i < n; i++) {
+//		for (j = 0; j < i; j++) {
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0; // 1361 별 계단 만들기
+//}
