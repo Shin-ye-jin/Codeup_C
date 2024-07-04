e494b3321610a75c47bb0777464466b015b19605
S510U <fx0605@naver.com>
2024-07-04 17:59:13 +0900

    // 0704

--- Codeup_C/07_02.c
+++ Codeup_C/07_02.c
@@ -61,3 +61,52 @@
 //	}
 //	return 0; //1405 숫자 로테이션
 //}
+
+//int main(void)
+//{
+//	char a[100];
+//	int i, j;
+//
+//	gets(a);
+//	
+//	for (i = 0; a[i]!='\0'; i++) {
+//		if (a[i] == ' ')
+//			continue;
+//		else
+//			printf("%c", a[i]);
+//	}
+//
+//	return 0; //1407 문자열 출력하기 1
+//}
+
+//int main(void)
+//{
+//	int n, i, j, str;
+//	int num[11];
+//
+//	for (i = 1; i <= 10; i++) {
+//		scanf("%d", &num[i]);
+//	}
+//	scanf("%d", &n);
+//	printf("1");
+//	printf("%d", num[n]);
+//
+//	return 0; // 1409 기억력 테스트 1
+//}
+
+//int main(void)
+//{
+//	int count1 = 0, count2 = 0, i, j;
+//	char a[100001];
+//
+//	gets(a);
+//
+//	for (i = 0; a[i] != '\0'; i++) {
+//		if (a[i] == '(')
+//			count1++;
+//		else if (a[i] == ')')
+//			count2++;
+//	}
+//	printf("%d %d", count1, count2);
+//	return 0; // 1410 올바른 괄호 1 (괄호 개수 세기)
+//}
