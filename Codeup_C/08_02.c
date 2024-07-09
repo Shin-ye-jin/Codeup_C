62ab3f972ee09af2cf140636d7bad222f309d455
S510U <fx0605@naver.com>
2024-07-09 20:37:27 +0900

    // 0709

---
+++ Codeup_C/08_02.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//int main(void)
+//{
+//	char a[101];
+//	int i, count = 0;
+//
+//	fgets(a, 101, stdin); // 문자열 입력 받기
+//
+//	for (i = 0; a[i]!='\0'; i++) {
+//		if (a[i] == 'l' && a[i+1] == 'o' && a[i+2] == 'v' && a[i+3] == 'e') {
+//			count++;
+//		}
+//	}
+//	printf("%d", count);
+//	return 0; // 1419 love 2
+//}
+
+//int main(void)
+//{
+//	char a[10];
+//
+//	scanf("%s", a);
+//
+//	if (a[0] == 'I' && a[1] == 'O' && a[2] == 'I'&& a[3]=='\0') {
+//		printf("IOI is the International Olympiad in Informatics.");
+//	}
+//	else {
+//		printf("I don't care.");
+//	}
+//	return 0; //1733 I.O.I
+//}
+
+//int main(void)
+//{
+//	char ID[11];
+//	scanf("%s", ID);
+//	printf("welcome! %s", ID);
+//	return 0; // 1734 welcome!
+//}
