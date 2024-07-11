ae069928cd22a3835fc177140667bc4124b214e1
S510U <fx0605@naver.com>
2024-07-11 22:15:55 +0900

    // 0711

---
+++ Codeup_C/08_04.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//int main() {
+//    int n, i, j, count = 1;
+//    int num[100][100];
+//
+//    scanf("%d", &n);
+//
+//    for (i = 0; i < n; i++) {
+//        for (j = 0; j < n; j++) {
+//            num[i][j] = count;
+//            printf("%d ", num[i][j]);
+//            count++;
+//        }
+//        printf("\n");
+//    }
+//
+//    return 0; // 1460 [기초-배열연습] 2차원 배열 순서대로 채우기 1-1
+//}
+
