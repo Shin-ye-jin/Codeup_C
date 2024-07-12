d7e4fcc6ffc17dce62b52a98cc3483121870fea6
S510U <fx0605@naver.com>
2024-07-12 21:20:03 +0900

    // 0714

---
+++ Codeup_C/08_06.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//int main() {
+//    int n, i, j, count = 0;
+//    int num[100][100];
+//
+//    scanf("%d", &n);
+//
+//    for (i = 0; i < n; i++) {
+//        if (i % 2 == 0) {
+//            count += n;
+//            for (j = 0; j < n; j++) {
+//                printf("%d ", count);
+//                count--;
+//            }
+//        }
+//        else {
+//            count += (n + 1);
+//            for (j = 0; j < n; j++) {
+//                printf("%d ", count);
+//                count++;
+//            }
+//            count--;
+//        }
+//        printf("\n");
+//    }
+//    return 0; // 1469 [기초-배열연습] 2차원 배열 지그재그 채우기 2-2
+//}
+
