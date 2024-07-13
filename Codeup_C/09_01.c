c5674b7624da35a4c31b129991645a8e8df1f506
S510U <fx0605@naver.com>
2024-07-13 12:06:51 +0900

    // 0715

--- Codeup_C/09_01.c
+++ Codeup_C/09_01.c
@@ -31,32 +31,62 @@
 //    return 0; // 1470 [기초-배열연습] 2차원 배열 지그재그 채우기 2-3
 //}
 
-int main() {
-    int n, i, j, count = 0;
-    int matrix[100][100];
+//int main() {
+//    int n, i, j, count = 0;
+//    int matrix[100][100];
+//
+//    scanf("%d", &n);
+//
+//    for (i = 0; i < n; i++) {
+//        if (i % 2 == 0) {
+//            for (j = n - 1; j >= 0; j--) {
+//                count++;
+//                matrix[i][j] = count;
+//            }
+//        }
+//        else {
+//            for (j = 0; j < n; j++) {
+//                count++;
+//                matrix[i][j] = count;
+//            }
+//        }
+//    }
+//
+//    for (i = 0; i < n; i++) {
+//        for (j = 0; j < n; j++) {
+//            printf("%d ", matrix[j][i]);
+//        }
+//        printf("\n");
+//    }
+//    return 0; // 1471 [기초-배열연습] 2차원 배열 지그재그 채우기 2-4
+//}
 
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++) {
-        if (i % 2 == 0) {
-            for (j = n - 1; j >= 0; j--) {
-                count++;
-                matrix[i][j] = count;
-            }
-        }
-        else {
-            for (j = 0; j < n; j++) {
-                count++;
-                matrix[i][j] = count;
-            }
-        }
-    }
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%d ", matrix[j][i]);
-        }
-        printf("\n");
-    }
-    return 0; // 1471 [기초-배열연습] 2차원 배열 지그재그 채우기 2-4
-}
+//int main(void)
+//{
+//	int n, m, i, j, count = 0;
+//	int matrix[100][100];
+//
+//	scanf("%d %d", &n, &m);
+//
+//	for (i = 0; i < n; i++) {
+//		if (i % 2 == 1) {
+//			for (j = 0; j < m; j++) {
+//				count++;
+//				matrix[i][j] = count;
+//			}
+//		}
+//		else {
+//			for (j = m - 1; j >= 0; j--) {
+//				count++;
+//				matrix[i][j] = count;
+//			}
+//		}
+//	}
+//
+//	for (i = n - 1; i >= 0; i--) {
+//		for (j = 0; j < m; j++) {
+//			printf("%d ", matrix[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
