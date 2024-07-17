cb7ad407642c471e5f3edd70a58e7fb265f7e886
S510U <fx0605@naver.com>
2024-07-17 19:32:38 +0900

    // 0717

---
+++ Codeup_C/09_03.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main() {
+//    int n, m, k, i, j, count = 0;
+//    int matrix[100][100];
+//
+//    scanf("%d %d", &n, &m);
+//
+//    for (i = 0; i < n + m - 1; i++) {
+//        for (j = 0; j < m; j++) {
+//            for (k = 0; k < n; k++) {
+//                if (j + k == i) {
+//                    count++;
+//                    matrix[k][j] = count;
+//                }
+//            }
+//        }
+//    }
+//
+//    for (i = 0; i < n; i++) {
+//        for (j = 0; j < m; j++) {
+//            printf("%d ", matrix[i][j]);
+//        }
+//        printf("\n");
+//    }
+//
+//    return 0; // 1476 [기초-배열연습] 2차원 배열 빗금 채우기 3-2
+//}
+
+//int main() {
+//    int n, m, k, i, j, count = 0;
+//    int matrix[100][100];
+//
+//    scanf("%d %d", &n, &m);
+//
+//    for (i = 0; i < n + m - 1; i++) {
+//        for (j = 0; j < n; j++) {
+//            for (k = 0; k < m; k++) {
+//                if (j + k == i) {
+//                    count++;
+//                    matrix[j][k] = count;
+//                }
+//            }
+//        }
+//    }
+//
+//    for (i = 0; i < n; i++) {
+//        for (j = 0; j < m; j++) {
+//            printf("%d ", matrix[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0; // 1477 [기초-배열연습] 2차원 배열 빗금 채우기 3-2
+//}
+
+//int main() {
+//    int n, m, k, i, j, count = 0;
+//    int matrix[100][100];
+//
+//    scanf("%d %d", &n, &m);
+//
+//    for (i = 0; i < n + m - 1; i++) {
+//        for (j = 0; j < n; j++) {
+//            for (k = 0; k < m; k++) {
+//                if (j + k == i) {
+//                    count++;
+//                    matrix[j][k] = count;
+//                }
+//            }
+//        }
+//    }
+//
+//    for (i = 0; i < n; i++) {
+//        for (j = m - 1; j >= 0; j--) {
+//            printf("%d ", matrix[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0; // 1478 [기초-배열연습] 2차원 배열 빗금 채우기 3-3
+//}
