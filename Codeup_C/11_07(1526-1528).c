60c18331aa3134565d921f8760a0e43d77fcc3a5
S510U <fx0605@naver.com>
2024-08-04 22:24:10 +0900

    // 0804

---
+++ Codeup_C/11_07(1526-1528).c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//void f() {
+//	printf("hello");
+//	return;
+//}
+//
+//int main() {
+//	f();
+//	return 0; // 1526 [기초-함수작성] 함수로 hello 문자열 출력하기
+//}
+
+
+//void f() {
+//	printf("123");
+//	return;
+//}
+//
+//int main() {
+//	f();
+//	return 0; // 1527 [기초-함수작성] 함수로 123 값 출력하기
+//}
+
+
+//void f() {
+//	printf("*");
+//	return;
+//}
+//
+//int main() {
+//	f();
+//	return 0; // 1528 [기초-함수작성] 함수로 *문자 출력하기
+//}
