646a05d22ca43126cd0c3d32baa346d059de792a
S510U <fx0605@naver.com>
2024-08-04 22:37:58 +0900

    // 0805

---
+++ Codeup_C/12_01.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//void f() {
+//	printf("**");
+//	return;
+//}
+//
+//int main() {
+//	f();
+//	return 0; // 1529 [기초-함수작성] 함수로 **문자 출력하기
+//}
+
+//char f() {
+//	return 'A';
+//}
+//
+//int main() {
+//	printf("%c", f());
+//	return 0; // 1530 [기초-함수작성] 함수로 문자 A 리턴하기
+//}
+
+//int f() {
+//	return 1;
+//}
+//
+//int main() {
+//	printf("%d", f());
+//	return 0; //1531 [기초-함수작성] 함수로 정수 (int) 1 리턴하기
+//}
