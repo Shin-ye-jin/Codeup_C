e18ece2f274d72893016db3fc4b676f9a495899d
S510U <fx0605@naver.com>
2024-08-04 22:49:20 +0900

    // 0806

---
+++ Codeup_C/12_01(1529-1534).c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//void f() {
+//	printf("**");
+//	return;
+//}
+//
+//int main() {
+//	f();
+//	return 0; // 1529 [기초-함수작성] 함수로 **문자 출력하기
+//}
+
+//char f() {
+//	return 'A';
+//}
+//
+//int main() {
+//	printf("%c", f());
+//	return 0; // 1530 [기초-함수작성] 함수로 문자 A 리턴하기
+//}
+
+//int f() {
+//	return 1;
+//}
+//
+//int main() {
+//	printf("%d", f());
+//	return 0; //1531 [기초-함수작성] 함수로 정수 (int) 1 리턴하기
+//}
+
+//long long int f() {
+//	return -2147483649LL;
+//}
+//
+//int main()
+//{
+//	printf("%lld", f());
+//	return 0; // 1532 [기초-함수작성] 함수로 정수(long long int) -2147483649 리턴하기
+//}
+
+//float f() {
+//	return 3.14f; // 실수의 마지막에 f를 붙이면 float형 상수로 처리된다.
+//}
+//
+//int main()
+//{
+//	printf("%f", f());
+//	return 0; // 1533 [기초-함수작성] 함수로 실수(float) 3.14 리턴하기
+//}
+
+
+//double f()
+//{
+//	return 3.1415926535897;
+//}
+//
+//int main()
+//{
+//	printf("%.13lf", f());
+//	return 0; // 1534 [기초-함수작성] 함수로 실수(double) 3.1415926535897 리턴하기
+//}
