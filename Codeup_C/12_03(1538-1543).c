b54dbc2b54fc3fcedb470686ae69fb3fdd4ba6f1
S510U <fx0605@naver.com>
2024-08-06 14:07:26 +0900

    // 0809

---
+++ Codeup_C/12_03(1538-1543).c
@@ -0,0 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int n;
+
+//void f(int num)
+//{
+//	if (num % 2 == 0) {
+//		printf("even");
+//	}
+//	else {
+//		printf("odd");
+//	}
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1538 [기초-함수작성] 함수로 odd 또는 even 출력하기
+//}
+
+//void f(int num)
+//{
+//	if (num == 0)
+//		printf("false");
+//	else
+//		printf("true");
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1539 [기초-함수작성] 함수로 false 또는 true 출력하기
+//}
+
+//void f(int num)
+//{
+//	if (num == 0)
+//		printf("zero");
+//	else
+//		printf("non zero");
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1540 [기초-함수작성] 함수로 zero 또는 non zero 출력하기
+//}
+
+//void f(int num) {
+//	if (num < 0) {
+//		printf("negative");
+//	}
+//	else if (num == 0) {
+//		printf("zero");
+//	}
+//	else {
+//		printf("positive");
+//	}
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1541 [기초-함수작성] 함수로 negative/zero/positive 출력하기
+//}
+
+
+//void f(int num) {
+//	int i, count = 0;
+//	for (i = 2; i <= 1000; i++) {
+//		if (num % i == 0)
+//			count++;
+//	}
+//
+//	if (count == 1)
+//		printf("prime");
+//	else
+//		printf("composite");
+//
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1542 [ 기초-함수작성] 함수로 prime 또는 composite 출력하기
+//}
+
+//void f(int num)
+//{
+//	int i;
+//	for (i = 0; i < num; i++) {
+//		printf("love\n");
+//	}
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1543 [기초-함수작성] 함수로 love 출력하기
+//}
