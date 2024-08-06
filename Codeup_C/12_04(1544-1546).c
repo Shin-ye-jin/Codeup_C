888a7b6b74f964b059d99c68a0f4de1099964bc6
S510U <fx0605@naver.com>
2024-08-06 14:49:18 +0900

    // 0810

---
+++ Codeup_C/12_04(1544-1546).c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+// int n;
+
+//void f(int num) {
+//	int i;
+//	for (i = 0; i < num; i++) {
+//		printf("*");
+//	}
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1544 [기초-함수작성] 함수로 *n개 출력하기
+//}
+
+//#include<stdbool.h>
+//bool zero(int k) {
+//	if (k == 0)
+//		return true;
+//	else
+//		return false;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	if (zero(n))printf("zero");
+//	else printf("non zero");
+//	return 0; // 1545 [기초-함수작성] 함수로 true(1) / false(0) 리턴하기
+//}
+
+//#include<stdbool.h>
+//bool zero(int k) {
+//	if (k == 0)
+//		return true;
+//	else
+//		return false;
+//}
+//
+//bool plus(int k) {
+//	return k > 0;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	if (zero(n)) printf("zero");
+//	else printf("%s", plus(n) ? "plus" : "minus");
+//	return 0; // 1546 [기초-함수작성] 함수로 plus/minus/0 판별하기
+//}
