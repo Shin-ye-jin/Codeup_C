ab19df54596d05cc2bcce5bb7130dcce86909242
S510U <fx0605@naver.com>
2024-08-06 15:02:18 +0900

    // 0811

---
+++ Codeup_C/12_05.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+// int n;
+
+//#include<stdbool.h>
+//bool prime(int k)
+//{
+//	int count = 0, i;
+//	for (i = 2; i <= 1000000; i++) {
+//		if (k % i == 0)
+//			count++;
+//	}
+//
+//	if (count == 1)
+//		return true;
+//	else
+//		return false;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	if (prime(n)) printf("prime");
+//	else printf("composite");
+//	return 0; // 1547 [기초-함수작성] 함수로 prime/composite 판별하기
+//}
+
+//char grade(int k) {
+//	if (k >= 90)
+//		return 'A';
+//	else if (k >= 80)
+//		return 'B';
+//	else if (k >= 70)
+//		return 'C';
+//	else if (k >= 60)
+//		return 'D';
+//	else
+//		return 'F';
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	printf("%c", grade(n));
+//	return 0; // 1548 [기초-함수작성] 함수로 학점 리턴하기
+//}
+
+//long long int n;
+//
+//long long int abs(long long int n) { // 변수형
+//	if (n < 0)
+//		return n * (-1);
+//	else
+//		return n;
+//}
+//
+//int main()
+//{
+//	scanf("%lld", &n);
+//	printf("%lld\n", abs(n));
+//	return 0; // 1549 [기초-함수작성] 함수로 절댓값 리턴하기
+//}
