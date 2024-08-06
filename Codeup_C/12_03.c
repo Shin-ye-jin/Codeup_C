c6f5e3bb8c385417ff309bc7d09c1fd0b6ccac34
S510U <fx0605@naver.com>
2024-08-06 13:52:06 +0900

    // 0808

---
+++ Codeup_C/12_03.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int n;
+
+//void f(int num)
+//{
+//	if (num % 2 == 0) {
+//		printf("even");
+//	}
+//	else {
+//		printf("odd");
+//	}
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1538 [기초-함수작성] 함수로 odd 또는 even 출력하기
+//}
+
+//void f(int num)
+//{
+//	if (num == 0)
+//		printf("false");
+//	else
+//		printf("true");
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1539 [기초-함수작성] 함수로 false 또는 true 출력하기
+//}
+
+//void f(int num)
+//{
+//	if (num == 0)
+//		printf("zero");
+//	else
+//		printf("non zero");
+//	return;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//	f(n);
+//	return 0; // 1540 [기초-함수작성] 함수로 zero 또는 non zero 출력하기
+//}
+
