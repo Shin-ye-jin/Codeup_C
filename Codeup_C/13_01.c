bb960682cb6ced88577edc1c322d6d49e2bbf59f
S510U <fx0605@naver.com>
2024-08-19 21:56:07 +0900

    // 0819

---
+++ Codeup_C/13_01.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//int n, k, d[1010];
+//
+//int upper_bound(int k)
+//{
+//	int i;
+//
+//	for (i = 1; i <= n; i++) {
+//		if (d[i] > k)
+//			return i;
+//	}
+//	return n + 1;
+//}
+//
+//int main()
+//{
+//	scanf("%d", &n);
+//
+//	for (int i = 1; i <= n; i++)
+//		scanf("%d", &d[i]);
+//
+//	scanf("%d", &k);
+//
+//	printf("%d\n", upper_bound(k));
+//
+//	return 0; // 1571 [기초-함수작성] 함수로 Upper Bound 위치 리턴하기
+//}
+
+
+//void f() {
+//	printf("Hello?");
+//}
+//
+//main()
+//{
+//	f();
+//	// 1576 (함수작성) void형 함수
+//}
+
+//int myabs(int a) {
+//	if (a < 0)
+//		return a * (-1);
+//	else
+//		return a;
+//}
+//
+//main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	printf("%d", myabs(a));
+//	// 1577 (함수 작성) 절댓값 함수 1
+//}
