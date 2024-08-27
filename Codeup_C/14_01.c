04d56a4f1117c64e9e52b9eabc1b295ce5bb2a5c
S510U <fx0605@naver.com>
2024-08-27 22:28:10 +0900

    // 0827

--- Codeup_C/14_01.c
+++ Codeup_C/14_01.c
@@ -58,3 +58,64 @@
 //	f(a, b);
 //	return 0; // 1904 (재귀함수) 두 수 사이의 홀수 출력하기
 //}
+
+//int f(int n) {
+//	if (n == 1)
+//		return 1;
+//	else {
+//		return n + f(n - 1);
+//	}
+//}
+//
+//int main(void)
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d", f(n));
+//	return 0; // 1905 (재귀함수) 1부터 n까지 합 구하기
+//}
+
+//int f(int n) {
+//	if (n == 1)
+//		return 1;
+//	else
+//		return n * f(n - 1);
+//}
+//
+//int main(void)
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d", f(n));
+//	return 0; // 1912 (재귀함수) 팩토리얼 계산
+//}
+
+//int f(int n) {
+//	if (n == 0) return 0; // 첫항을 0
+//	else if (n <= 1)return 1; // 두번째 항을 1
+//	return f(n - 1) + f(n - 2); // 그 다음 항 값은 앞에 두 항의 합
+//}
+//
+//int main(void)
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d", f(n));
+//	return 0; // 1915 (재귀함수) 피보나치 수열
+//}
+
+//int a[201];
+// // 메모이제이션 컴퓨터 프로그램이 동일한 계산을 반복해야 할 때, 이전에 계산한 값을 메모리에 저장함으로써 동일한 계산 반복 수행을 제거
+//int f(int n) {
+//	if (a[n]!=0) return a[n]; // 이미 해결한 값이면 배열에 있는 값을 리턴한다. if(a[n])
+//	else if (n <= 2)return 1;
+//	return a[n] = (f(n - 1) + f(n - 2)) % 10009;
+//}
+//
+//int main(void)
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d", f(n));
+//	return 0; // 1916 (재귀함수) 피보나치 수열 (Large)
+//}
